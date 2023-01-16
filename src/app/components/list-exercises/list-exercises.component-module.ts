97d91a2fa885e43dcdebe07e3103cb91742e18ee
KarolinaUser <karolina.krakow@o2.pl>
2023-01-16 21:47:27 +0100

    List exercises - single part cz.2

--- src/app/components/list-exercises/list-exercises.component-module.ts
+++ src/app/components/list-exercises/list-exercises.component-module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { ListExercisesComponent } from './list-exercises.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
-  imports: [CommonModule, MatListModule, MatChipsModule],
+  imports: [CommonModule, MatListModule, MatChipsModule, MatCheckboxModule, MatCardModule ],
   declarations: [ListExercisesComponent],
   providers: [],
   exports: [ListExercisesComponent]
--- src/app/components/list-exercises/list-exercises.component.ts
+++ src/app/components/list-exercises/list-exercises.component.ts
@@ -5,7 +5,9 @@ import {
 } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { ListModel } from '../../models/list.model';
+import { ProductModel } from '../../models/product.model';
 import { ListsService } from '../../services/lists.service';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-list-exercises',
@@ -15,9 +17,15 @@ import { ListsService } from '../../services/lists.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ListExercisesComponent {
+  
   name$: Observable<string[]> = of(['Tom', 'Andrew', 'Chris', 'Peter']);
 
   readonly crypto$: Observable<ListModel[]> = this._listsService.getAll();
 
-  constructor(private _listsService: ListsService) {}
+  readonly product$: Observable<ProductModel[]> = this._productService.getAll();
+
+  constructor(
+    private _listsService: ListsService,
+    private _productService: ProductService
+  ) {}
 }
