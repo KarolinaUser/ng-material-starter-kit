986338d00da6a45462ed448662d5a36ac96dfd97
KarolinaUser <karolina.krakow@o2.pl>
2023-01-18 13:32:43 +0100

    Form - Control - Email

--- src/app/components/forms-example/forms-example.component.ts
+++ src/app/components/forms-example/forms-example.component.ts
@@ -16,7 +16,6 @@ export class FormsExampleComponent {
     name: new FormControl('', [Validators.required, Validators.minLength(3)])
 
   });
-
   onFormNameSubmitted(formName: FormGroup): void {
     console.log(formName.value)
 
@@ -26,7 +25,14 @@ export class FormsExampleComponent {
   readonly age: FormGroup = new FormGroup({
     age: new FormControl('18', [Validators.required, Validators.min(18)])
   });
-
   onAgeSubmitted(age: FormGroup): void {
+  }
+
+
+  readonly emailForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9]+@[a-z0-9.-]+\\.[a-z]{2,}$')])
+  });
+
+  onEmailFormSubmitted(emailForm: FormGroup): void {
   }
 }
